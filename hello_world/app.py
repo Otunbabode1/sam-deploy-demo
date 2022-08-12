aa734e1c0fcef1a87e4c43d25cc3f3ea70a64c7d
Abolaji Olabode <abolaji.olabode@slalom.com>
2022-08-12 11:53:34 -0400

    fix

--- hello_world/app.py
+++ hello_world/app.py
@@ -5,24 +5,18 @@ from urllib import request
 
 
 def lambda_handler(event, context):
-    body = json.loads(event["body"])
-    firstname = body["firstname"]
-    lastname = body["lastname"]
-    fullname = f"{firstname} {lastname}"
-    try:
-        ip = request.Request.get("http://checkip.amazonaws.com/")
-    except request.RequestException as e:
-        #Send some context about this error to Lambda Logs
-        print(e)
+   body = json.loads(event['body'])
+   firstname = body["firstname"]
+   lastname = body["lastname"]
+   consultant = {
+    "FirstName": firstname,
+    "LastName": lastname,
+    "Role": "Consultant"
 
-        raise e
-    #1223
-    return {
-        "statusCode": 200,
-        "body": json.dumps(
-            {
-                "message": fullname,
-                # "location": ip.text.replace("\n", "")
-            }
-        ),
-    }
+   }
+
+
+   return {
+    'statusCode': 200,
+    'body': json.dumps(consultant)
+   } 
